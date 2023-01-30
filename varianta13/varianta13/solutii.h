5cc980009e4c7c4e46f73a1f398088932b1cfac8
Sandru Gabriel <gabi200611@yahoo.com>
2023-01-30 23:38:20 +0200

    done

---
+++ varianta13/varianta13/solutii.h
@@ -0,0 +1,131 @@
+﻿#pragma once
+#include "functii.h"
+
+
+/* Scrieţi ce se afişează pe
+ecran în urma executării
+secvenţei de program
+alăturate, în care variabila s
+memorează un şir de cel mult
+12 caractere, iar variabila i
+este de tip întreg. (6p.)
+char s[13]="abcdefghoid";
+i=0;
+cout<<strlen(s); | printf("%d",strlen(s));
+while (i<strlen(s))
+ if (strchr("aeiou",s[i])!=NULL)
+ strcpy(s+i,s+i+1);
+ else i++;
+cout<<" "<<s; | printf(" %s",s); 
+*/
+void solutia1() {
+
+	char s[13] = "abcdefghoid";
+	int i = 0;
+	cout << strlen(s);
+	while (i < strlen(s))
+		if (strchr("aeiou", s[i]) != NULL)
+			strcpy(s + i, s + i + 1);
+		else i++;
+	cout << " " << s;
+	//11 bcdfghd
+}
+
+/*Scrieţi un program C/C++ care citeşte de la tastatură un număr natural n (2<n<25) şi apoi
+construieşte în memorie o matrice cu n linii şi n coloane, numerotate de la 1 la n, ale cărei
+elemente primesc valori după cum urmează:
+- elementele aflate pe diagonala secundară sunt toate nule;
+- elementele de pe coloana i (1≤i≤n), aflate deasupra diagonalei secundare, au valoarea
+egală cu i;
+- elementele de pe linia n-i+1 (1≤i≤n), aflate sub diagonala secundară, au valoarea egală
+cu i.
+5.
+Programul afişează pe ecran matricea construită, câte o linie a matricei pe
+câte o linie a ecranului, elementele fiecărei linii fiind separate prin câte un
+spaţiu.
+Exemplu: pentru n=4 se va afişa matricea alăturată. (10p.)
+1 2 3 0
+1 2 0 3
+1 0 2 2
+0 1 1 1*/
+void solutia2() {
+	int n = 4;
+	generare(n);
+}
+
+/*Fie subprogramul fct definit alăturat, parţial. Iniţial, variabile întregi
+a, b şi c au valorile a=8, b=31 şi c=9, iar după apelul fct(a,b,c),
+valorile celor trei variabile sunt a=9, b=31 şi c=39. Care poate fi
+antetul subprogramului fct? (4p.)
+void fct(....)
+{ x=x+1;
+ y=y-1;
+ z=x+y;
+}
+a. void fct(int &x,int &y,int &z) b. void fct(int x,int &y,int &z)
+c. void fct(int x,int y,int z) d. void fct(int &x,int y,int &z) 
+*/
+void solutia3() {
+
+	//d. void fct(int& x, int y, int& z)
+}
+
+/* Se consideră şirul 1, 2,1, 3,2,1, 4,3,2,1, ...
+construit astfel: prima grupă este formată din numărul 1, a doua grupă este formată din
+numerele 2 şi 1, iar grupa a k-a, este formată din numerele k, k-1,..., 1.
+Se cere să se citescă de la tastatură un număr natural n (n≤1000) şi să se afişeze pe ecran
+cel de al n-lea termen al şirului dat.
+a) Descrieţi un algoritm de rezolvare a acestei probleme, eficient din punct de vedere al
+timpului de executare şi al spaţiului de memorie, explicând în ce constă eficienţa acestuia.
+(4p.)
+b) Scrieţi programul C/C++ corespunzător algoritmului descris*/
+void solutia4() {
+	int n = 134;
+	afisareNtermin(n);
+}
+
+/*Se consideră subprogramul P care are doi parametri:
+– n, prin intermediul căruia primeşte un număr natural de cel mult 9 cifre
+– c, prin intermediul căruia primeşte o cifră.
+Subprogramul va furniza tot prin intermediul parametrului n numărul obţinut din n prin
+eliminarea tuturor apriţiilor cifrei c. Dacă, după eliminare, numărul nu mai conţine nicio cifră
+sau conţine doar cifre 0, rezultatul returnat va fi 0.
+a) Scrieţi doar antetul subprogramului P. (2p.)
+b) Pe prima linie a fişierului text BAC.IN se găsesc, separate prin câte un spaţiu, mai multe
+numere naturale de cel mult 9 cifre fiecare. Scrieţi programul C/C++ care citeşte numerele
+din acest fişier, utilizând apeluri ale subprogramului P elimină toate cifrele impare din fiecare
+dintre aceste numere şi apoi scrie în fişierul text BAC.OUT numerele astfel obţinute,
+separate prin câte un spaţiu. Dacă un număr din fişierul BAC.IN nu conţine nicio cifră pară
+nenulă, acesta nu va mai apărea deloc în fişierul de ieşire. (8p.)
+Exemplu: dacă fişierul BAC.IN conţine numerele 25 7 38 1030 45127 0 35 60 15,
+atunci BAC.OUT va avea conţinutul: 2 8 42 60.*/
+void solutia5() {
+
+	afisare();
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
