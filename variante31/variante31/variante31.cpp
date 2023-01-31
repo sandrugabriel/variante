e96bcac0755e8f615d97a02ce23d011ce972bc4f
Sandru Gabriel <gabi200611@yahoo.com>
2023-01-31 23:38:45 +0200

    done

--- variante31/variante31/variante31.cpp
+++ variante31/variante31/variante31.cpp
@@ -1,11 +1,11 @@
 // variante31.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include <iostream>
+#include "solutii.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	solutia5();
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
