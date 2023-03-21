56e1a1076a971232911b6f9b1135fd116548922c
Sandru Gabriel <gabi200611@yahoo.com>
2023-03-21 18:57:31 +0200

    done

---
+++ varianta94/varianta94/solutii.h
@@ -0,0 +1,78 @@
+﻿#pragma once
+#include "functii.h"
+
+
+
+/*3. Scrieţi definiţia completă a funcţiei f, care primeşte prin intermediul parametrului n un număr
+natural nenul (2≤n≤200), iar prin intermediul parametrului a un tablou unidimensional care
+conţine n valori întregi, fiecare dintre aceste valori întregi având cel mult patru cifre. Funcţia
+returnează valoarea 1 dacă elementele tabloului formează un şir crescător, valoarea 2 dacă
+elementele tabloului formează un şir descrescător, valoarea 0 dacă elementele tabloului
+formează un şir constant şi valoarea -1 în rest. */
+void solutia1() {
+    int a[100] = {1,13,54,25,12};
+    cout << f(5, a);
+
+}
+
+/*4. Fişierul text număr.txt conţine pe prima linie o valoare naturală n cu exact 9 cifre nenule
+distincte. Scrieţi un program eficient din punctul de vedere al timpului de executare care
+citeşte din fişier numărul n şi afişează pe ecran cea mai mică valoare m formată din exact
+aceleaşi cifre ca şi n, astfel încât m>n. În cazul în care nu există o astfel de valoare,
+programul va afişa pe ecran mesajul Nu exista.
+Exemplu: Dacă fişierul număr.txt conţine numărul 257869431, se va afişa pe ecran
+numărul 257891346.
+ a) Descrieţi succint, în limbaj natural, metoda de rezolvare folosită, explicând în ce constă
+eficienţa ei (3 – 4 rânduri). (4p.)
+b) Scrieţi un program C/C++ care rezolvă problema conform metodei descrise. */
+void solutia2() {
+
+    afisarea();
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
