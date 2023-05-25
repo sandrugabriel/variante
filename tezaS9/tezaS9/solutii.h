1bb4252fb0d843ddb4c9a951f12021946af286bb
Sandru Gabriel <gabi200611@yahoo.com>
2023-05-25 15:22:39 +0300

    done

---
+++ tezaS9/tezaS9/solutii.h
@@ -0,0 +1,56 @@
+﻿#pragma once
+#include "functii.h"
+
+
+/*for (i=0; i<5; i++) { for(j=0; j<5; j++)
+{....
+cout<<endl; }
+0 1 2 3 4
+2 3 4 5 6
+0 1 2 3 4
+6 7 8 9 10
+0 1 2 3 4*/
+void solutia1() {
+	generare();
+}
+
+/*
+2. Se consideră un număr natural n (2<n<50) și un tablou unidimensional a, format din n numere naturale nenule 
+şi distincte, fiecare număr având cel mult 9 cifre. Scrieți un program C++ care determină şi afişează numărul 
+de perechi distincte formate din elemente din tabloul a, prime între ele. Două numere naturale sunt prime 
+între ele dacă au un singur divizor comun: numărul 1.
+Exemplu: Dacă n = 6 şi a = (42, 3, 14, 43, 8, 6) atunci se va obține valoarea 7, deoarece vectorul are 7 
+perechi de numere ce verifică proprietatea dată: (42, 43); (3, 14); (3, 43); (10 puncte) (3, 8); 
+(14, 43); (43, 8) ;(43, 6).*/
+void solutia2(){
+	int n = 6, a[100] = { 42, 3, 14, 43, 8, 6 };
+	afisare(n, a);
+}
+
+/*
+3. Fişierul bac.txt conține pe prima linie un număr natural n, care este multiplu de 7 (7<n<70000) şi 
+pe a doua linie un șir de n numere întregi nenule, având cel mult trei cifre. Şirul de numere se 
+împarte în secvențe de câte 7 numere denumite benzi și pentru fiecare bandă se determină cele mai 
+mici două numere distincte min1 şi min2. Scrieți un program
+Bacalaureat}C++, eficient ca timp de execuție și spațiu de memorie utilizat, care afişează pe 
+ecran, pentru fiecare dintre benzi, în ordine crescătoare cele două valori min1 şi min2 sau numărul
+0, dacă banda nu conține două valori distincte min1 şi min2.
+Exemplu: Pentru n = 21, şirul de numere -3, 10, 1, 2, -5, 6, -5, 1, 1, 1, 1, 1, 1, 1, 10.9.1. 2, 3, 6, 7 se 
+va afişa -5.-3, 0, 1, 2.*/
+void solutia3() {
+
+	afisarea();
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
